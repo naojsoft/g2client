6bb2feb7a6a536b43dacfd10f85d0c1cb1d2abf9
Eric Jeschke <eric@naoj.org>
2023-01-04 17:53:13 -1000

    Update to using setuptools_scm, pyproject.toml and setup.cfg

--- g2client/version.py
+++
@@ -1,7 +0,0 @@
-# this file was automatically generated
-major = 2
-minor = 2
-release = 20180822021334
-
-version = '%d.%d.%d' % (major, minor, release)
-
--- setup.py
+++ setup.py
@@ -1,35 +1,8 @@
 #! /usr/bin/env python
 #
-from g2client.version import version
-import os
+# Licensed under a 3-clause BSD style license - see LICENSE.md
+#
 
-srcdir = os.path.dirname(__file__)
+from setuptools import setup
 
-try:
-    from setuptools import setup
-
-except ImportError:
-    from distutils.core import setup
-
-setup(
-    name = "g2client",
-    version = version,
-    author = "Software Division, Subaru Telescope, NAOJ",
-    author_email = "ocs@naoj.org",
-    description = ("A client for observing with the Subaru Telescope Observation Control System."),
-    license = "BSD",
-    keywords = "subaru, telescope, observation, client",
-    url = "http://naojsoft.github.com/g2client",
-    packages = ['g2client',
-                'g2client.icons',
-                ],
-    package_data = {'g2client.icons':['*.png']},
-    scripts = ['scripts/soundsink', 'scripts/g2disp', 'scripts/g2disp_gui'],
-    classifiers = [
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX",
-        "Topic :: Scientific/Engineering :: Astronomy",
-    ],
-)
+setup()
